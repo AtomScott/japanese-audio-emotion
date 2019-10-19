307cbd7df996c5f371e552c094ddf931fe3bf536
atom <atomscott>
2019-10-19 17:49:26 +0900

    Added scrape function

---
+++ tools/arguments.py
@@ -0,0 +1,63 @@
+import argparse
+
+def parse_args():
+    """Custom argument parser for command line usage.
+    
+    Returns:
+        args {argparse.Namespace} -- dict of arguments
+    """
+    parser = argparse.ArgumentParser()
+
+    # General
+    parser.add_argument(
+        '--debug',
+        default=False,
+        action='store_true',
+        help='Outputs stuff for debugging'
+    )
+
+    #########################
+    # * Args for Web Scraping
+    #########################
+
+    # General
+    parser.add_argument(
+        '--query', 
+        type=str,
+        default='Elon Musk',
+        help='Search query')
+    parser.add_argument(
+        '--n_images',
+        type=int,
+        default=30,
+        help='Number of images to download')
+    parser.add_argument(
+        '--out_dir_path',
+        type=str,
+        default='../datasets/XperFace/',
+        help='path to image dir')
+    parser.add_argument(
+        '--chromedriver_path',
+        type=str,
+        default='./chromedriver',
+        help='path to chrome_driver')
+    parser.add_argument(
+        '--overwrite',
+        default=False,
+        action='store_true',
+        help='Overwrite output dir'
+    )
+
+    # Experimental
+
+
+    #######################################
+    # * Args for Facial Emotion Recognition
+    #######################################
+
+    # General
+
+    # Experimental
+
+    args = parser.parse_args()
+    return args
--- tools/get_talking_sequence.py
+++ tools/get_talking_sequence.py
@@ -1,6 +1,59 @@
-def main():
-    pass
+from arguments import parse_args
+from logger import create_logger
+from tests import Tester
+
+from google_images_download import google_images_download
+import os
+import shutil
+
+
+
+def download_images(query, n_images, out_dir_path, chromedriver_path="./chromedriver"):
+    """Downloads face images for a query.
+    
+    Arguments:
+        query {str} -- Query to download images for.
+        n_images {int} -- Number of images to download.
+        out_dir_path {str} -- Output path for images.
+    
+    Keyword Arguments:
+        chromedriver_path {str} -- Path to chromedriver (default: {"./chromedriver"})
+    
+    Returns:
+        [list] -- list of absolute paths to images
+    """
+    assert os.path.isfile(chromedriver_path), logger.critical("chromedriver not found")
+    assert os.access(chromedriver_path, os.X_OK), logger.critical("chromedriver has incorrect permissions")
+
+    response = google_images_download.googleimagesdownload()
+
+    args = {
+        "keywords": query,
+        "limit": n_images,
+        "format": "jpg",
+        "type": "face",
+        "output_directory": out_dir_path,
+        "chromedriver": "./chromedriver",
+        "silent_mode": True
+    }
+    out_image_paths = response.download(args)[0][query]
+    if len(out_image_paths) == 0:
+        out_image_paths = download_images(query, n_images, out_dir_path, chromedriver_path)
+
+    return out_image_paths
 
 
 if __name__ =="__main__":
-    main()
+    args = parse_args()
+    logger = create_logger()
+    tester = Tester(args.debug)
+
+    if args.overwrite and os.path.isdir(args.out_dir_path):
+        shutil.rmtree(args.out_dir_path)
+    os.makedirs(args.out_dir_path, exist_ok=True)
+
+    scraped_image_paths = download_images(args.query, args.n_images, os.path.join(args.out_dir_path, 'images'), args.chromedriver_path)
+
+    test = lambda x: 0 < len(x) <= args.n_images
+    tester.test_list(scraped_image_paths, test)
+
